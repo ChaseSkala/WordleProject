faa28f6f2a933ebcf6fd61dae6c3f9bcd7ef6e55
chaseskala <Chase@cgcg.biz>
2025-05-22 19:24:54 -0400

    Updated the sorting inside gather_information and created a dictionary of spots where letters that are in the word but in the incorrect spot cannot be.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 import json
 import random
 
-in_correct_spot = []
+in_correct_spot = [None, None, None, None, None]
 in_word_not_spot = []
 not_in_word = []
+forbidden_spots = {}
 
 def create_session() -> str:
   url = "http://127.0.0.1:8000/session"
@@ -22,17 +23,19 @@ def make_guess(session_id: str):
 
   with open('words.txt', 'r') as file:
     words: list[str] = [line.strip() for line in file]
-    random_word: str = random.choice(words)
+    if in_correct_spot == [None, None, None, None, None] and in_word_not_spot == [] and not_in_word == []:
+      word: str = random.choice(words)
+
 
   payload = json.dumps({
-    "guess": f"{random_word}"
+    "guess": f"{word}"
   })
   headers = {
     'Content-Type': 'application/json'
   }
 
   response = requests.request("POST", url, headers=headers, data=payload)
-
+  print(f"Guessed word: {word}")
   print(response.text)
   return response.json()
 
@@ -45,15 +48,26 @@ def gather_information(guess):
 
   for letter_info in data['letters']:
     if letter_info["in_correct_spot"]:
-      in_correct_spot.append(letter_info["letter"])
+      for idx, letter_info in enumerate(data['letters']):
+        if letter_info["in_correct_spot"]:
+          in_correct_spot[idx] = letter_info["letter"]
     if letter_info["in_word"] and not letter_info["in_correct_spot"]:
-      in_word_not_spot.append(letter_info["letter"])
+      for pos, letter_info in enumerate(data['letters']):
+        if letter_info["in_word"] and not letter_info["in_correct_spot"]:
+          letter = letter_info["letter"]
+      if letter in forbidden_spots:
+        forbidden_spots[letter].append(pos)
+      else:
+        forbidden_spots[letter] = [pos]
+    if letter_info["letter"] in in_word_not_spot and letter_info["in_correct_spot"]:
+      in_word_not_spot.remove(letter_info["letter"])
     if not letter_info["in_word"] and not letter_info["in_correct_spot"]:
       not_in_word.append(letter_info["letter"])
 
   print(in_correct_spot)
   print(in_word_not_spot)
   print(not_in_word)
+  print(forbidden_spots)
 
 def main():
   session_id = create_session()
