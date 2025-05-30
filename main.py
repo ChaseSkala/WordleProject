f4523471f80743ddbfff15bd6a43c8f49b36de47
chaseskala <Chase@cgcg.biz>
2025-05-30 19:00:21 -0400

    Restructuring and optimization.

--- main.py
+++ main.py
@@ -18,35 +18,6 @@ def create_session() -> str:
 
   return response.json()['session_id']
 
-def matches_correct_spots(word, guess_info):
-  for i, letter in enumerate(guess_info.in_correct_spot):
-    if letter is not None and word[i] != letter:
-      return False
-  return True
-
-def respects_forbidden_spots(word: str, guess_info: GuessData) -> bool:
-  for letter, forbidden_indices in guess_info.forbidden_spots.items():
-    for idx in forbidden_indices:
-      if idx < len(word) and word[idx] == letter:
-        return False
-  return True
-
-def no_incorrect_letters(word: str, guess_info: GuessData) -> bool:
-  for letter in guess_info.not_in_word:
-    if letter in word:
-      return False
-  return True
-
-
-def is_wordle_found(word, guess_info):
-  for i, letter in enumerate(guess_info.in_correct_spot):
-    if letter is None or word[i] != letter:
-      return False
-  return True
-
-def filter_words(word: str, guess_info: GuessData) -> bool:
-  return matches_correct_spots(word, guess_info) and respects_forbidden_spots(word, guess_info) and no_incorrect_letters(word, guess_info)
-
 def make_guess(session_id: str, guess_info: GuessData) -> tuple[list[LetterInfo], str]:
   url = f"http://127.0.0.1:8000/session/{session_id}/guess"
 
@@ -59,7 +30,7 @@ def make_guess(session_id: str, guess_info: GuessData) -> tuple[list[LetterInfo]
         word: str = random.choice(words)
         found_new_word = True
       else:
-        words = list(filter(lambda w: filter_words(w, guess_info), words))
+        words = list(filter(lambda w: GuessData.filter_words(w), words))
         word: str = random.choice(words)
         found_new_word = True
 
@@ -79,26 +50,6 @@ def make_guess(session_id: str, guess_info: GuessData) -> tuple[list[LetterInfo]
     letter_infos.append(LetterInfo(**letter_info))
   return letter_infos, word
 
-def gather_information(guess: list[LetterInfo], guess_info: GuessData):
-  for idx, letter_info in enumerate(guess):
-    if letter_info.in_correct_spot:
-      guess_info.in_correct_spot[idx] = letter_info.letter
-    if letter_info.in_word and not letter_info.in_correct_spot:
-      if letter_info.letter in guess_info.forbidden_spots:
-        guess_info.forbidden_spots[letter_info.letter].append(idx)
-      else:
-        guess_info.forbidden_spots[letter_info.letter] = [idx]
-    if letter_info.letter in guess_info.in_word_not_spot and letter_info.in_correct_spot:
-      guess_info.in_word_not_spot.remove(letter_info.letter)
-    if not letter_info.in_word and not letter_info.in_correct_spot:
-      if letter_info.letter not in guess_info.not_in_word:
-        guess_info.not_in_word.append(letter_info.letter)
-
-  print(guess_info.in_correct_spot)
-  print(guess_info.in_word_not_spot)
-  print(guess_info.forbidden_spots)
-  print(guess_info.not_in_word)
-
 def main():
   finding_wordle = True
   while finding_wordle:
@@ -109,18 +60,18 @@ def main():
     session_id = create_session()
     print(f"Attempt {attempts}")
     guess, word = make_guess(session_id, guess_data)
-    gather_information(guess, guess_data)
+    GuessData.gather_information(guess)
     while not ready_to_exit:
       attempts += 1
       if attempts == max_attempts:
         ready_to_exit = True
-      if is_wordle_found(word, guess_data):
+      if GuessData.is_wordle_found(word):
         ready_to_exit = True
         found_wordle = True
       else:
         print(f"Attempt {attempts}")
         guess, word = make_guess(session_id, guess_data)
-        gather_information(guess, guess_data)
+        GuessData.gather_information(guess)
     if found_wordle:
       print(f"Today's wordle is {word}")
       finding_wordle = False
--- modules.py
+++ modules.py
@@ -13,3 +13,64 @@ class GuessData:
     in_word_not_spot = []
     not_in_word = []
     forbidden_spots = {}
+
+    def matches_correct_spots(word: str) -> bool:
+        for i, letter in enumerate(GuessData.in_correct_spot):
+            if letter is not None and word[i] != letter:
+                return False
+        return True
+
+    def respects_forbidden_spots(word: str) -> bool:
+        for letter, forbidden_indices in GuessData.forbidden_spots.items():
+            for idx in forbidden_indices:
+                if idx < len(word) and word[idx] == letter:
+                    return False
+        return True
+
+    def no_incorrect_letters(word: str) -> bool:
+        for letter in GuessData.not_in_word:
+            if letter in word:
+                return False
+        return True
+
+    def is_wordle_found(word: str):
+        for i, letter in enumerate(GuessData.in_correct_spot):
+            if letter is None or word[i] != letter:
+                return False
+        return True
+
+    def use_forbidden_word_in_different_spot(word: str) -> bool:
+        for letter, forbidden_indices in GuessData.forbidden_spots.items():
+            if letter not in word:
+                return False
+            if all(word[idx] == letter for idx in forbidden_indices):
+                return False
+        return True
+
+    def filter_words(word: str) -> bool:
+        return (
+                GuessData.matches_correct_spots(word)
+                and GuessData.respects_forbidden_spots(word)
+                and GuessData.no_incorrect_letters(word)
+                and GuessData.use_forbidden_word_in_different_spot(word)
+        )
+
+    def gather_information(guess: list[LetterInfo]):
+        for idx, letter_info in enumerate(guess):
+            if letter_info.in_correct_spot:
+                GuessData.in_correct_spot[idx] = letter_info.letter
+            if letter_info.in_word and not letter_info.in_correct_spot:
+                if letter_info.letter in GuessData.forbidden_spots:
+                    GuessData.forbidden_spots[letter_info.letter].append(idx)
+                else:
+                    GuessData.forbidden_spots[letter_info.letter] = [idx]
+            if letter_info.letter in GuessData.in_word_not_spot and letter_info.in_correct_spot:
+                GuessData.in_word_not_spot.remove(letter_info.letter)
+            if not letter_info.in_word and not letter_info.in_correct_spot:
+                if letter_info.letter not in GuessData.not_in_word:
+                    GuessData.not_in_word.append(letter_info.letter)
+
+        print(GuessData.in_correct_spot)
+        print(GuessData.in_word_not_spot)
+        print(GuessData.forbidden_spots)
+        print(GuessData.not_in_word)
